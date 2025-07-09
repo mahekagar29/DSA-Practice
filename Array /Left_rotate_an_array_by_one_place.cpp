a2578a2b7cf660affe04a92c3a975339fe6b4a08
Mahek Agarwal <118847439+mahekagar29@users.noreply.github.com>
2025-07-09 20:16:59 +0530

    Create Left_rotate_an_array_by_one_place.cpp

---
+++ Array /Left_rotate_an_array_by_one_place.cpp
@@ -0,0 +1,21 @@
+//Left rotate an array by one place
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> arr;
+    int a;
+    cin>>a;
+    for(int i=0;i<a;i++){
+        int n;
+        cin>>n;
+        arr.push_back(n);
+    }
+    int temp=arr[0];
+    for(int i=0;i<a-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[a-1]=temp;
+    for(int i=0;i<a;i++){
+        cout<<arr[i]<<" ";
+    }
+}
